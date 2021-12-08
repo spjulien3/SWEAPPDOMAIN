dd12604ddd376dbbbdad94e75e2bf53d2387f26c
Sam Julien <sjulien@students.kennesaw.edu>
2021-12-07 19:52:48 -0500

    database part 2

--- versatech/ledgerAccount/models.py
+++ versatech/ledgerAccount/models.py
@@ -6,36 +6,36 @@ from djmoney.models.fields import MoneyField
 import uuid
 
 NORMAL_SIDE_CHOICES = (
-    ("1","RIGHT"),("2","LEFT"),
+    ("RIGHT","RIGHT"),("LEFT","LEFT"),
 )
 
 ACCOUNT_CATEGORY_CHOICES = (
-    ("1","ASSETS"), ("2","LIABILITIES"),
-    ("3","EQUITY"), ("4","INCOME"),
-    ("5","EXPENSE"), ("6","REVENUE"),
+    ("ASSETS","ASSETS"), ("LIABILITIES","LIABILITIES"),
+    ("EQUITY","EQUITY"), ("INCOME","INCOME"),
+    ("EXPENSE","EXPENSE"), ("REVENUE","REVENUE"),
 )
 
 ACCOUNT_SUBCATEGORY_CHOICES = (
-    ("1","CURRRENT ASSETS"), ("2","FIXED ASSETS"), 
-    ("3","LONG TERM ASSETS"), ("4","ACCOUNTS RECIEVABLE"),
-    ("5","CURRENT LIABILITIES"), ("6","PAYROLL LIABILITIES"),
-    ("7","LONG TERM LIABILITIES"), ("8","NOTES PAYABLE"),
-    ("9","ACCRUED EXPENSES"), ("10","DEFFERED REVENUE"),
-    ("11","COMMON STOCK"), ("12","PREFERRED STOCK"),
-    ("13","RETAINED EARNINGS"), ("14","DIVIDENDS"),
-    ("15","TREASURY STOCK"),
+    ("CURRRENT ASSETS","CURRRENT ASSETS"), ("FIXED ASSETS","FIXED ASSETS"), 
+    ("LONG TERM ASSETS","LONG TERM ASSETS"), ("ACCOUNTS RECIEVABLE","ACCOUNTS RECIEVABLE"),
+    ("CURRENT LIABILITIES","CURRENT LIABILITIES"), ("PAYROLL LIABILITIES","PAYROLL LIABILITIES"),
+    ("LONG TERM LIABILITIES","LONG TERM LIABILITIES"), ("NOTES PAYABLE","NOTES PAYABLE"),
+    ("ACCRUED EXPENSES","ACCRUED EXPENSES"), ("DEFFERED REVENUE","DEFFERED REVENUE"),
+    ("COMMON STOCK","COMMON STOCK"), ("PREFERRED STOCK","PREFERRED STOCK"),
+    ("RETAINED EARNINGS","RETAINED EARNINGS"), ("DIVIDENDS","DIVIDENDS"),
+    ("TREASURY STOCK","TREASURY STOCK"),
 )
 
 
 
 # Create your models here.
 class ledgerAccount(models.Model):
-    account_number = models.IntegerField(default=0)
+    account_number = models.CharField(max_length=3,default=0)
     account_name = models.CharField(max_length=50)
-    account_description = models.CharField(max_length=100)
+    account_description = models.CharField(max_length=100, blank=True )
     normal_side = models.CharField(max_length=5, choices=NORMAL_SIDE_CHOICES)
     account_category =  models.CharField(max_length=100, choices=ACCOUNT_CATEGORY_CHOICES)
-    account_subcategory = models.CharField(max_length=100, choices=ACCOUNT_SUBCATEGORY_CHOICES)
+    account_subcategory = models.CharField(max_length=100, choices=ACCOUNT_SUBCATEGORY_CHOICES, blank=True)
     debit = MoneyField(max_digits=14, decimal_places=2, default_currency='USD', blank=True)
     credit = MoneyField(max_digits=14, decimal_places=2, default_currency='USD', blank=True)
     balance = MoneyField(max_digits=14, decimal_places=2, default_currency='USD', blank=True)
